0a05928337e476067b6b530fc51707431766ff23
Yun <jmy.12k3@gmail.com>
2023-08-14 03:42:49 +0800

    update project/strategies/__init__.py

--- project/strategies/__init__.py
+++ project/strategies/__init__.py
@@ -5,7 +5,7 @@ from importlib.util import module_from_spec, spec_from_file_location
 def get_strategy(name):
     for dirpath, _, filenames in os.walk(os.path.dirname(__file__)):
         for filename in filenames:
-            if not (filename.endswith(".py") and filename.replace(".py") == name):
+            if not (filename.endswith(".py") and filename.replace(".py", "") == name):
                 continue
             location = os.path.join(dirpath, filename)
             spec = spec_from_file_location(name, location)
