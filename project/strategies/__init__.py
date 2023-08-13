3f4ea0d1b04a9b2fada13acba0bfe251ef24c660
Yun <jmy.12k3@gmail.com>
2023-08-14 03:36:17 +0800

    build(deps): update project/strategies/__init__.py and requirements.txt

--- project/strategies/__init__.py
+++ project/strategies/__init__.py
@@ -1,17 +1,15 @@
-import importlib
 import os
+from importlib.util import module_from_spec, spec_from_file_location
 
 
 def get_strategy(name):
     for dirpath, _, filenames in os.walk(os.path.dirname(__file__)):
-        filename: str
         for filename in filenames:
-            if filename.endswith(".py"):
-                if filename.replace(".py", "") == name:
-                    spec = importlib.util.spec_from_file_location(
-                        name, os.path.join(dirpath, filename)
-                    )
-                    module = importlib.util.module_from_spec(spec)
-                    spec.loader.exec_module(module)
-                    return module.Strategy
+            if not (filename.endswith(".py") and filename.replace(".py") == name):
+                continue
+            location = os.path.join(dirpath, filename)
+            spec = spec_from_file_location(name, location)
+            module = module_from_spec(spec)
+            spec.loader.exec_module(module)
+            return module.Strategy
     return None
