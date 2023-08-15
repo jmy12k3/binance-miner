4ec028498a2a638b0ba6d5ff43f6c7de04e03555
Yun <jmy.12k3@gmail.com>
2023-08-16 01:55:06 +0800

    update __main__.py

--- project/__main__.py
+++ project/__main__.py
@@ -1,4 +1,5 @@
 import contextvars
+import os
 
 from .crypto_trading import main
 
@@ -6,5 +7,7 @@ if __name__ == "__main__":
     ctx = contextvars.copy_context()
     try:
         ctx.run(main())
+    except TypeError:
+        os._exit(1)
     except KeyboardInterrupt:
         pass
