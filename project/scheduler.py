9515c5c6c90429730e6405c5f841eb6238c13572
Yun <jmy.12k3@gmail.com>
2023-08-20 00:22:27 +0800

    update scheduler.py

--- project/scheduler.py
+++ project/scheduler.py
@@ -1,13 +1,14 @@
-import logging
 import traceback
 from datetime import datetime
 
 from schedule import Job, Scheduler
 
+from .logger import AbstractLogger
+
 
 # https://gist.github.com/mplewis/8483f1c24f2d6259aef6?permalink_comment_id=3703372#gistcomment-3703372
 class SafeScheduler(Scheduler):
-    def __init__(self, logger: logging.Logger, rerun_immediately: bool = True):
+    def __init__(self, logger: AbstractLogger, rerun_immediately: bool = True):
         super().__init__()
         self.logger = logger
         self.rerun_immediately = rerun_immediately
