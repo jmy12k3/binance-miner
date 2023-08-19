a2a4754c6845ac131d618dcf0416f7dc5266e204
Yun <jmy.12k3@gmail.com>
2023-08-19 22:21:48 +0800

    update api_server.py

--- project/api_server.py
+++ project/api_server.py
@@ -3,11 +3,13 @@
 from datetime import datetime
 from enum import Enum
 from itertools import groupby
+from typing import Any
 
 from dateutil.relativedelta import relativedelta
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
+from fastapi_socketio import SocketManager
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.query import Query
@@ -17,7 +19,7 @@ from .config import Config
 from .database import Database
 from .logger import DummyLogger
 
-# Initialize FastAPI with CORS and SocketIO
+# Initialize FastAPI server
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -26,6 +28,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+sio = SocketManager(app)
 
 # Initialize database
 logger = DummyLogger("api_server")
@@ -63,29 +66,6 @@ async def redirect_to_docs():
     return RedirectResponse(url="/docs")
 
 
-@app.get("/api/v1/current_coin")
-def current_coin():
-    coin = db.get_current_coin()
-    return coin.info() if coin else None
-
-
-@app.get("/api/v1/coins")
-def coins():
-    session: Session
-    with db.db_session() as session:
-        _current_coin = session.merge(db.get_current_coin())
-        _coins: list[models.Coin] = session.query(models.Coin).all()
-        return [{**coin.info(), "is_current": coin == _current_coin} for coin in _coins]
-
-
-@app.get("/api/v1/pairs")
-def pairs():
-    session: Session
-    with db.db_session() as session:
-        all_pairs: list[models.Pair] = session.query(models.Pair).all()
-        return [pair.info() for pair in all_pairs]
-
-
 @app.get("/api/v1/value_history")
 def value_history(period: Period | None = None, coin: str | None = None):
     session: Session
@@ -115,14 +95,14 @@ def total_value_history(period: Period | None = None):
         return [{"datetime": tv[0], "btc": tv[1], "usd": tv[2]} for tv in total_values]
 
 
-@app.get("/api/v1/current_coin_history")
-def current_coin_history(period: Period | None = None):
+@app.get("/api/v1/trade_history")
+def trade_history(period: Period | None = None):
     session: Session
     with db.db_session() as session:
-        query = session.query(models.CurrentCoin)
-        query = filter_period(period, query, models.CurrentCoin)
-        current_coins: list[models.CurrentCoin] = query.all()
-        return [cc.info() for cc in current_coins]
+        query = session.query(models.Trade).order_by(models.Trade.datetime.asc())
+        query = filter_period(period, query, models.Trade)
+        trades: list[models.Trade] = query.all()
+        return [trade.info() for trade in trades]
 
 
 @app.get("/api/v1/scouting_history")
@@ -142,11 +122,40 @@ def scouting_history(period: Period | None = None):
         return [scout.info() for scout in scouts]
 
 
-@app.get("/api/v1/trade_history")
-def trade_history(period: Period | None = None):
+@app.get("/api/v1/current_coin")
+def current_coin():
+    coin = db.get_current_coin()
+    return coin.info() if coin else None
+
+
+@app.get("/api/v1/current_coin_history")
+def current_coin_history(period: Period | None = None):
     session: Session
     with db.db_session() as session:
-        query = session.query(models.Trade).order_by(models.Trade.datetime.asc())
-        query = filter_period(period, query, models.Trade)
-        trades: list[models.Trade] = query.all()
-        return [trade.info() for trade in trades]
+        query = session.query(models.CurrentCoin)
+        query = filter_period(period, query, models.CurrentCoin)
+        current_coins: list[models.CurrentCoin] = query.all()
+        return [cc.info() for cc in current_coins]
+
+
+@app.get("/api/v1/coins")
+def coins():
+    session: Session
+    with db.db_session() as session:
+        _current_coin = session.merge(db.get_current_coin())
+        _coins: list[models.Coin] = session.query(models.Coin).all()
+        return [{**coin.info(), "is_current": coin == _current_coin} for coin in _coins]
+
+
+@app.get("/api/v1/pairs")
+def pairs():
+    session: Session
+    with db.db_session() as session:
+        all_pairs: list[models.Pair] = session.query(models.Pair).all()
+        return [pair.info() for pair in all_pairs]
+
+
+@sio.on("update", namespace="/backend")
+async def on_update(_, msg: dict[str, Any]):
+    print(msg)
+    await sio.emit("update", msg, namespace="/frontend")
